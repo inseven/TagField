d1d8c3a63c517f907a7246cbfcbd377cb0f5d11d
Jason Morley <hello@jbmorley.co.uk>
2024-05-10 11:30:06 -1000

    First addition of the code from Thoughts

--- Package.swift
+++ Package.swift
@@ -1,21 +1,24 @@
 // swift-tools-version: 5.10
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "TagField",
+    platforms: [.iOS(.v17), .macOS(.v14)],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "TagField",
             targets: ["TagField"]),
     ],
+    dependencies: [
+        .package(url: "git@github.com:saramah/HashRainbow.git", branch: "main"),
+    ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "TagField"),
+            name: "TagField",
+            dependencies: [
+                .product(name: "HashRainbow", package: "HashRainbow"),
+            ]),
         .testTarget(
             name: "TagFieldTests",
             dependencies: ["TagField"]),
