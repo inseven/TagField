b5ebc57743634cc6f538c4b101869fc8bfa7403f
Jason Morley <hello@jbmorley.co.uk>
2024-05-10 16:00:21 -1000

    Build the example project

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,8 +7,17 @@ set -x
 SCRIPT_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 ROOT_DIRECTORY="${SCRIPT_DIRECTORY}/.."
 
+# Build the package.
+
 cd "$ROOT_DIRECTORY"
 
 xcodebuild -scheme TagField -showdestinations
 xcodebuild -scheme TagField -destination "platform=macOS" clean build
 # xcodebuild -scheme TagField -destination "platform=iOS Simulator,name=iPhone 14 Pro" clean build
+
+
+# Build the example project.
+
+cd "$ROOT_DIRECTORY/Example"
+
+xcodebuild -scheme Example -configuration Debug build
